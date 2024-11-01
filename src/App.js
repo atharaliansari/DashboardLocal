35155a866319dd88dfb7a391d325fd850e0e39b2
AtharIqbal <157898481+atharaliansari@users.noreply.github.com>
2024-11-01 19:47:41 +0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import AppRouter from './config/AppRouter/approuter';
+
+function App() {
+  return (
+    <div>
+      <AppRouter/>
+   
+    </div>
+  );
+}
+
+export default App;
